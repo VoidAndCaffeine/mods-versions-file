a393feaf7f3432f083f4b52ecd92de3464f1eb3c
Miska Alexia <miska.alexia@proton.me>
2023-07-02 22:23:51 -0700

    finalize format

--- stringout.java
+++ stringout.java
@@ -6,14 +6,19 @@ import java.io.ObjectOutputStream;
  * stringout
  */
 public class stringout {
-    private static String[][] sOut = new String[1][4];
+    private static String[][] sOut = new String[2][4];
     private static String filename = "mods.versions";
 
     public static void stringOut() throws IOException{
-        sOut[0][0] = "Sodium"; 
-        sOut[0][1] = "sodium.jar";
-        sOut[0][2] = "1200410"; // mc1.20-0.4.10
-        sOut[0][3] = "https://cdn.modrinth.com/data/AANobbMI/versions/vgceLbdH/sodium-fabric-mc1.20-0.4.10%2Bbuild.27.jar"; 
+        sOut[0][0] = "1"; 
+        sOut[0][1] = "mods.versions";
+        sOut[0][2] = "000";
+        sOut[0][3] = "https://raw.githubusercontent.com/VoidAndCaffeine/mods-versions-file/main/mods.versions"; 
+
+        sOut[1][0] = "Sodium"; 
+        sOut[1][1] = "sodium.jar";
+        sOut[1][2] = "0";
+        sOut[1][3] = "https://cdn.modrinth.com/data/AANobbMI/versions/vgceLbdH/sodium-fabric-mc1.20-0.4.10%2Bbuild.27.jar"; 
 
         saveStringFile();
     }
